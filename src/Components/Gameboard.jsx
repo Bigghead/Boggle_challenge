6392ecb893c009a5b1fcf26c16f0ac5a48b614be
Bigghead <shavenchew@yahoo.com>
2018-08-09 14:49:50 -0700

    build 2d array of random characters from dice

--- src/Components/Gameboard.jsx
+++ src/Components/Gameboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { allDice, shuffleBoard , getRandomChar} from '../utils/board'
+import { allDice, shuffleBoard , getRandomChar } from '../utils/board'
 
 
 const renderBoard = ( array ) => {
@@ -10,10 +10,24 @@ const renderBoard = ( array ) => {
 };
 
 
-const renderGrid = ( array ) => {
+/**
+ * 
+ * @param { array<string> } shuffledBoard array of already shuffled list of dice
+ * make an empty 2d array of 5 x 5
+ * for every cell in 2d array, put in an element from shuffledBoard ( the last in this case, since we pop )
+ */
+const renderGrid = ( shuffledBoard ) => {
+
     let emptyArr = Array.from( {length: 5 }, (_, i) => undefined ) ;
     let boardGrid = [];
-    for( let i = 0; i < 5; i ++ ) { boardGrid.push(emptyArr); }
+    
+    for( let i = 0; i < 5; i ++ ) { 
+        boardGrid.push(emptyArr.slice());
+        for( let j = 0; j < 5; j ++ ) {
+            let char = getRandomChar(shuffledBoard.shift());
+            boardGrid[i][j] = char;
+        }
+     }
     return boardGrid;
 }
 
